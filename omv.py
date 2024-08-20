568abdf3e8eb39938a15ad8d3edf2a10deb5d6c2
davidson :: bx0 <davidsonmizael@gmail.com>
2024-08-19 22:57:59 -0300

    Clearing screen at startup

--- omv.py
+++ omv.py
@@ -7,15 +7,15 @@ __import__("utils.versioncheck")
 from utils.banner import print_banner
 from core.enums import COLORS
 from core import wizard
-from utils.terminal import print_colored
+from utils.terminal import print_colored, clear_screen
 
 def shutdown():
     print_colored("\n[!] Exiting...", COLORS.RED)
     exit()
 
 try:
-
     if __name__ == "__main__":
+        clear_screen()
         print_banner()
         wizard.run()
 
